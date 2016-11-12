f61c013f3759705c836eb1f9625053b1d0c9f116
gspagoni <gspagoni@outlook.com>
2016-11-12 15:54:23 -0800

    fix the resource package

--- geafimrest/src/main/java/org/spago/gsi/geafimrest/resources/RegistryTypeResourceBasicAuth.java
+++ geafimrest/src/main/java/org/spago/gsi/geafimrest/resources/RegistryTypeResourceBasicAuth.java
@@ -13,7 +13,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
 import org.spago.gsi.geafimrest.model.RegistryType;
