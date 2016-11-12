7632f39dacb74d22c388a7518544aa1edb4bfcd8
gspagoni <gspagoni@outlook.com>
2016-11-12 15:56:14 -0800

    fixed BasicAuthFilter class

--- geafimrest/src/main/java/org/spago/gsi/geafimrest/filter/BasicAuthFilter.java
+++ geafimrest/src/main/java/org/spago/gsi/geafimrest/filter/BasicAuthFilter.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
